38dcc7429e77969f09c7974dfc0a5454ba31ef5b
Nathan Green <n8thangreen@yahoo.co.uk>
2016-11-03 15:53:42 +0000

    loop over all deterministic scenario parameter values

--- scripts/02-decision-tree-model.R
+++ scripts/02-decision-tree-model.R
@@ -45,95 +45,110 @@ for (i in seq_along(who_levels)){
 rm(i)
 
 
-# sensitivity analysis ----------------------------------------------------
-
-j <- 1
-
-# assign branching _probabilities_
-osNode.cost$Set(p = scenario_parameter_p[j, "Screening"],
-                filterFun = function(x) x$name=="Screening")
-osNode.cost$Set(p = scenario_parameter_p[j, "No Screening"],
-                filterFun = function(x) x$name=="No Screening")
-
-osNode.cost$Set(p = scenario_parameter_p[j, "Start Treatment"],
-                filterFun = function(x) x$name=="Start Treatment")
-osNode.cost$Set(p = scenario_parameter_p[j, "Not Start Treatment"],
-                filterFun = function(x) x$name=="Not Start Treatment")
-
-osNode.cost$Set(p = scenario_parameter_p[j, "Complete Treatment"],
-                filterFun = function(x) x$name=="Complete Treatment")
-osNode.cost$Set(p = scenario_parameter_p[j, "Not Complete Treatment"],
-                filterFun = function(x) x$name=="Not Complete Treatment")
-
-osNode.health$Set(p = scenario_parameter_p[j, "Screening"],
-                  filterFun = function(x) x$name=="Screening")
-osNode.health$Set(p = scenario_parameter_p[j, "No Screening"],
-                  filterFun = function(x) x$name=="No Screening")
-
-osNode.health$Set(p = scenario_parameter_p[j, "Start Treatment"],
-                  filterFun = function(x) x$name=="Start Treatment")
-osNode.health$Set(p = scenario_parameter_p[j, "Not Start Treatment"],
-                  filterFun = function(x) x$name=="Not Start Treatment")
-
-osNode.health$Set(p = scenario_parameter_p[j, "Complete Treatment"],
-                  filterFun = function(x) x$name=="Complete Treatment")
-osNode.health$Set(p = scenario_parameter_p[j, "Not Complete Treatment"],
-                filterFun = function(x) x$name=="Not Complete Treatment")
-
-# assign branching _costs_
-osNode.cost$Set(distn = "unif",
-                filterFun = function(x) x$name=="Agree to Screen")
-osNode.cost$Set(min = scenario_parameter_cost[j, "Agree to Screen"],
-                filterFun = function(x) x$name=="Agree to Screen")
-osNode.cost$Set(max = scenario_parameter_cost[j, "Agree to Screen"],
-                filterFun = function(x) x$name=="Agree to Screen")
-
-
-# pathway probabilities ---------------------------------------------------
-
-# calculate total probabilities along each branch, from root to leaf
-## screening
-path_probs.screen <- treeSimR::calc_pathway_probs(osNode.cost)
-osNode.cost$Set(path_probs = path_probs.screen)
-
-# print(osNode.cost, "type", "p", "path_probs", "distn",
-#       "mean", "sd", "min", "max", "a", "b", "shape", "scale", limit = NULL)
-
-# test sum to 1
-sum(osNode.cost$Get("path_probs", filterFun = isLeaf))
-
-##TODO##
-## this is a bit messy because the screening (uncertain) event is in between the WHO and LTBI events on the tree
-## and this may be different for different groups
-## theres probably a better way to do this:
-##    with data.tree operations?
-##    re-order tree structure?
-
-# total probability successfully complete treatment of LTBI
-# use when know active TB cases in advance
-p.complete_treatment <- osNode.cost$Get('path_probs',
-                                        filterFun = function(x) x$name=="Complete Treatment" & !grepl(pattern = "non-LTBI", x$pathString))
-
-# branch probabilities for LTBI
-p.LTBI <- osNode.cost$Get('p',
-                          filterFun = function(x) x$name=="LTBI" & !grepl(pattern = "No Screening", x$pathString))
-
-# prob of completing treatment for LTBI individuals in each WHO category
-p.complete_treat_given_LTBI_by_who <- setNames(p.complete_treatment/(p.LTBI * p.who), nm = who_levels)
-
-
-rm(p.LTBI, p.complete_treatment)
-
-
-# expected values ---------------------------------------------------------
-
 N.mc <- 10
 
-# for defined nodes, sample expected cost
-mc.cost <- treeSimR::MonteCarlo_expectedValues(osNode = osNode.cost, n = N.mc)
 
-# for defined nodes, sample expected health
-mc.health <- treeSimR::MonteCarlo_expectedValues(osNode.health, n = N.mc)
+# sensitivity analysis ----------------------------------------------------
+# iterate over each deterministic scenario of parameter values
+j <- 1
 
-# rm(N.mc)
+for (j in 1:nrow(scenario_parameter_cost)){
+
+  # assign branching _probabilities_
+  osNode.cost$Set(p = scenario_parameter_p[j, "Screening"],
+                  filterFun = function(x) x$name=="Screening")
+  osNode.cost$Set(p = scenario_parameter_p[j, "No Screening"],
+                  filterFun = function(x) x$name=="No Screening")
+
+  osNode.cost$Set(p = scenario_parameter_p[j, "Start Treatment"],
+                  filterFun = function(x) x$name=="Start Treatment")
+  osNode.cost$Set(p = scenario_parameter_p[j, "Not Start Treatment"],
+                  filterFun = function(x) x$name=="Not Start Treatment")
+
+  osNode.cost$Set(p = scenario_parameter_p[j, "Complete Treatment"],
+                  filterFun = function(x) x$name=="Complete Treatment")
+  osNode.cost$Set(p = scenario_parameter_p[j, "Not Complete Treatment"],
+                  filterFun = function(x) x$name=="Not Complete Treatment")
+
+  osNode.health$Set(p = scenario_parameter_p[j, "Screening"],
+                    filterFun = function(x) x$name=="Screening")
+  osNode.health$Set(p = scenario_parameter_p[j, "No Screening"],
+                    filterFun = function(x) x$name=="No Screening")
+
+  osNode.health$Set(p = scenario_parameter_p[j, "Start Treatment"],
+                    filterFun = function(x) x$name=="Start Treatment")
+  osNode.health$Set(p = scenario_parameter_p[j, "Not Start Treatment"],
+                    filterFun = function(x) x$name=="Not Start Treatment")
+
+  osNode.health$Set(p = scenario_parameter_p[j, "Complete Treatment"],
+                    filterFun = function(x) x$name=="Complete Treatment")
+  osNode.health$Set(p = scenario_parameter_p[j, "Not Complete Treatment"],
+                    filterFun = function(x) x$name=="Not Complete Treatment")
+
+  # assign branching _costs_
+  osNode.cost$Set(distn = "unif",
+                  filterFun = function(x) x$name=="Agree to Screen")
+  osNode.cost$Set(min = scenario_parameter_cost[j, "Agree to Screen"],
+                  filterFun = function(x) x$name=="Agree to Screen")
+  osNode.cost$Set(max = scenario_parameter_cost[j, "Agree to Screen"],
+                  filterFun = function(x) x$name=="Agree to Screen")
+
+
+  # pathway probabilities ---------------------------------------------------
+
+  # calculate total probabilities along each branch, from root to leaf
+  ## screening
+  path_probs.screen <- treeSimR::calc_pathway_probs(osNode.cost)
+  osNode.cost$Set(path_probs = path_probs.screen)
+
+  # print(osNode.cost, "type", "p", "path_probs", "distn",
+  #       "mean", "sd", "min", "max", "a", "b", "shape", "scale", limit = NULL)
+
+  # test sum to 1
+  sum(osNode.cost$Get("path_probs", filterFun = isLeaf))
+
+  ##TODO##
+  ## this is a bit messy because the screening (uncertain) event is in between the WHO and LTBI events on the tree
+  ## and this may be different for different groups
+  ## theres probably a better way to do this:
+  ##    with data.tree operations?
+  ##    re-order tree structure?
+
+  # total probability successfully complete treatment of LTBI
+  # use when know active TB cases in advance
+  p.complete_Tx <- osNode.cost$Get('path_probs',
+                                   filterFun = function(x) x$name=="Complete Treatment" & !grepl(pattern = "non-LTBI", x$pathString))
+
+  # branch probabilities for LTBI
+  p.LTBI <- osNode.cost$Get('p',
+                            filterFun = function(x) x$name=="LTBI" & !grepl(pattern = "No Screening", x$pathString))
+
+  # prob of completing treatment for LTBI individuals in each WHO category
+  p.complete_Tx_given_LTBI_by_who <- setNames(p.complete_Tx/(p.LTBI * p.who), nm = who_levels)
+
+
+
+  # expected values ---------------------------------------------------------
+
+  # for defined nodes, sample expected cost
+  mc.cost <- treeSimR::MonteCarlo_expectedValues(osNode = osNode.cost, n = N.mc)
+
+  # for defined nodes, sample expected health
+  mc.health <- treeSimR::MonteCarlo_expectedValues(osNode.health, n = N.mc)
+
+
+  ########
+  # save #
+  ########
+
+  # cost-effectiveness outputs
+  cat(x = as.numeric(mc.cost$`expected values`),
+      file = "mc_cost.csv", append = TRUE, fill = TRUE)
+  cat(x = as.numeric(mc.health$`expected values`),
+      file = "mc_health.csv", append = TRUE, fill = TRUE)
+
+  # pathway probabilities
+  cat(x = as.numeric(p.complete_Tx_given_LTBI_by_who),
+      file = "prob_complete_Tx_given_LTBI_by_who.csv", append = TRUE, fill = TRUE)
+}
 
