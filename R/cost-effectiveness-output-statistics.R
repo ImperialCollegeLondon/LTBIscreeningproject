3d4f928da0812b0685bc308a820f8924be55355e
Nathan Green <n8thangreen@yahoo.co.uk>
2017-01-17 16:09:58 +0000

    change ICER and INMB functions to take e and c rather than the differences
    based this on the bcea function

--- R/cost-effectiveness-output-statistics.R
+++ R/cost-effectiveness-output-statistics.R
@@ -3,8 +3,9 @@
 #'
 #' Differences are [intervention] - [status-quo].
 #'
-#' @param delta.e Difference in effectivness i.e. health e.g. QALYs
-#' @param delta.c Difference in costs
+#' @param e effectivness i.e. health e.g. QALYs
+#' @param c costs
+#' @param ref reference column. Defaults to 1
 #' @param wtp Willingness to pay threshold
 #'
 #' @return Value
@@ -12,12 +13,28 @@
 #'
 #' @examples
 #'
-calc.INMB <- function(delta.e, delta.c, wtp = 20000){
+calc.INMB <- function(e, c, ref = 1, wtp = 20000){
 
   if(wtp<0)
     stop("Willingness to pay must be non-negative.")
 
-  return((delta.e * wtp) - delta.c)
+  if(any(dim(c)!=dim(e)))
+    warning("Dimensions of e and c do not match.")
+
+  n.sim <- dim(e)[1]
+  n.comparators <- dim(e)[2]
+
+  # Define reference & comparator intervention
+  ints <- 1:n.comparators
+  comp <- ints[-ref]
+  n.comparisons <- n.comparators - 1
+
+  delta.e <- as.data.frame(e[ ,comp] - e[ ,ref])
+  delta.c <- as.data.frame(c[ ,comp] - c[ ,ref])
+
+  INMB <- (colMeans(delta.e) * wtp) - colMeans(delta.c)
+
+  return(INMB)
 }
 
 
