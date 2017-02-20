12fedc9fa4d9850ffd09ccfd501a3a2dba85a291
Nathan Green <n8thangreen@yahoo.co.uk>
2017-02-20 16:12:25 +0000

    * bug fix: exit look of count_comprsk_events() when the risk set is empty. Needed to then add back in the repeated empty risk set years to the time horizon.

--- R/count_comprsk_events.R
+++ R/count_comprsk_events.R
@@ -7,8 +7,11 @@
 #' In cases where two events happen in the same time interval
 #' the first event in the list argument is used.
 #'
+#' If this we're the case then it would be easier to calculate.
+#'
 #' @param indiv_event_times List of individual-level time series
 #' @param followup_max_year Time horizon
+#' @param fill Append counts up to time horixon after risk-set empty
 #'
 #' @return Event counts by year
 #' @export
@@ -25,7 +28,8 @@
 #' detach(IMPUTED_sample_year_cohort)
 #'
 count_comprsk_events <- function(indiv_event_times,
-                                 followup_max_year = 100){
+                                 followup_max_year = 100,
+                                 fill = TRUE){
 
   if(missing(indiv_event_times)){
     stop("Require event times")
@@ -44,9 +48,9 @@ count_comprsk_events <- function(indiv_event_times,
 
   # replace missing event with after time horizon
   indiv_event_times <- lapply(indiv_event_times,
-                              function(x) replace(x, is.na(x), 101))
+                              function(x) replace(x, is.na(x), 1001))
 
-  N_pop <- length(indiv_event_times[[1]])
+  N_pop <- n_events[1]
 
   event_counts <- NULL
 
@@ -54,22 +58,41 @@ count_comprsk_events <- function(indiv_event_times,
 
     # FALSE means removed from the population
     risk_set <- rep(TRUE, N_pop)
-    event_year <- NULL
+    num_event_year <- NULL
 
     for (eventi in seq_along(indiv_event_times)){
 
+      # of the at-risk pop who experiences eventi before yeari
       event_lgl <- indiv_event_times[[eventi]][risk_set]<yeari
+
+      # remove recent event indiv from risk set
       risk_set[risk_set][event_lgl] <- FALSE
-      event_year <- c(event_year, sum(event_lgl))
+
+      num_new_events <- sum(event_lgl)
+      num_event_year <- c(num_event_year,
+                          num_new_events)
     }
 
     event_counts <- cbind(event_counts, c(yeari,
-                                          event_year,
-                                          N_pop - sum(event_year)))
+                                          num_event_year,
+                                          N_pop - sum(num_event_year)))
+
+    # risk-set = 0
+    if (N_pop - sum(num_event_year)<=0) break
   }
+
+  # fill to followup_max_year
+  # last value carried forward
+  num_years_events <- ncol(event_counts)
+  missing_years <- matrix(event_counts[, num_years_events, drop = FALSE],
+                          byrow = FALSE,
+                          nrow = nrow(event_counts),
+                          ncol = followup_max_year - num_years_events)
+  event_counts <- cbind(event_counts, missing_years)
+
 
   rownames(event_counts) <- c("year",
                               names(indiv_event_times),
-                              "remainder")
+                              "at-risk")
   return(event_counts)
 }
