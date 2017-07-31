571a859311981ad6435f675bb3e7388a7b761775
Dr Nathan Green <nathan.green@imperial.ac.uk>
2017-07-31 13:19:16 +0100

    catch exceptions in scenario loop

--- scripts/000-programme-level-params-scenario-runner.R
+++ scripts/000-programme-level-params-scenario-runner.R
@@ -12,8 +12,13 @@ devtools::load_all(".")
 data("global-parameters-scenarios")
 data("global-parameters-scenarios_ls")
 
+sources_correctly <- NULL
+
 for (global_run in 1:length(global_params_scenarios_ls)) {
 
   print(sprintf("[ programme level parameters ] scenario: %d", global_run))
-  source("scripts/00-main.R")
+
+  try_error <- try(source("scripts/00-main.R"))
+
+  sources_correctly <- c(try_error, !inherits(sources_correctly, "try-error"))
 }
