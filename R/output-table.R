ec25c4c49bf4e9633d37ebfa39a397bfe328ae29
Dr Nathan Green <nathan.green@imperial.ac.uk>
2017-08-02 11:34:31 +0100

    reorder output table

--- R/output-table.R
+++ R/output-table.R
@@ -6,18 +6,19 @@
 #' @export
 #'
 #' @examples
-table_costeffectiveness <- function(bcea_obj) {
+table_costeffectiveness <- function(bcea_obj,
+                                    wtp_threshold) {
 
   attach(bcea_obj)
 
   out <- do.call(data.frame,
-                 list("percentile_95pc" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.95),
-                      "percentile_5pc" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.05),
+                 list("5th_percentile" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.05),
                       "EINB" = eib[k == wtp_threshold, ],
-                      "pCE_WTP15000" = apply(ib[k == 15000, , ], 2, function(x) sum(x > 0)/n.sim),
-                      "pCE_WTP20000" = apply(ib[k == 20000, , ], 2, function(x) sum(x > 0)/n.sim),
-                      "pCE_WTP25000" = apply(ib[k == 25000, , ], 2, function(x) sum(x > 0)/n.sim),
-                      "pCE_WTP30000" = apply(ib[k == 30000, , ], 2, function(x) sum(x > 0)/n.sim)))
+                      "95th_percentile" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.95),
+                      "pCE_WTP15000" = apply(ib[k == 15000, , ], 2, function(x) sum(x >= 0)/n.sim),
+                      "pCE_WTP20000" = apply(ib[k == 20000, , ], 2, function(x) sum(x >= 0)/n.sim),
+                      "pCE_WTP25000" = apply(ib[k == 25000, , ], 2, function(x) sum(x >= 0)/n.sim),
+                      "pCE_WTP30000" = apply(ib[k == 30000, , ], 2, function(x) sum(x >= 0)/n.sim)))
 
   detach(bcea_obj)
 
