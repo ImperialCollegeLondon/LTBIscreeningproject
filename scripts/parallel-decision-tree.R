b26e0ee69e0cbdaee355ca9698667da52fae3a89
Dr Nathan Green <nathan.green@imperial.ac.uk>
2017-08-09 13:39:36 +0100

    parallelised version of the cluster script using local cores

---
+++ scripts/parallel-decision-tree.R
@@ -0,0 +1,64 @@
+#****************************************************************
+# LTBI screening model
+# N Green
+# Aug 2017
+#
+# https://www.r-bloggers.com/how-to-go-parallel-in-r-basics-tips/
+
+
+library(parallel)
+
+if (getwd() != "Q:/R/cluster--LTBI-decision-tree") {
+  exit_wd <- getwd()}
+
+source("scripts/cluster-prep-decisiontree-data.R")
+
+setwd("Q:/R/cluster--LTBI-decision-tree")
+
+sources <- list.files(pattern = "[.]R$")[!grepl(x = list.files(pattern = "[.]R$"), pattern = "^cluster-master")]
+
+source("decision_tree_cluster.R")
+
+
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+
+# Initiate cluster
+cl <- makeCluster(no_cores)
+
+n.uk_tb <- unlist(n.uk_tb)
+n.exit_tb <- unlist(n.exit_tb)
+
+clusterExport(cl, "n.uk_tb")
+clusterExport(cl, "n.exit_tb")
+clusterExport(cl, "N.mc")
+
+clusterEvalQ(cl, library(data.tree))
+clusterEvalQ(cl, library(dplyr))
+clusterEvalQ(cl, library(treeSimR))
+
+set.seed(12345)
+
+ptm <- proc.time()
+
+res <- parLapply(cl, scenario_parameters, decision_tree_cluster,
+                 N.mc = N.mc,
+                 n.uk_tb = n.uk_tb,
+                 n.exit_tb = n.exit_tb)
+
+proc.time() - ptm
+
+stopCluster(cl)
+
+saveRDS(res, file = cluster_output_filename)
+
+setwd(exit_wd)
+
+
+##to debug
+# res <- lapply(scenario_parameters, decision_tree_cluster,
+#               N.mc = N.mc,
+#               n.uk_tb = n.uk_tb,
+#               n.exit_tb = n.exit_tb)
+
+
