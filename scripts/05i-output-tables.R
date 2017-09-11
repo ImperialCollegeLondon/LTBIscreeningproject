5721d0028908e5928e63671f026e1652aec60565
Dr Nathan Green <nathan.green@imperial.ac.uk>
2017-09-11 10:58:16 +0100

    numbers of tb avoided output table

--- scripts/05i-output-tables.R
+++ scripts/05i-output-tables.R
@@ -1,11 +1,28 @@
-#
+# *****************************
 # LTBI screening
 # N Green
 # July 2017
+#
+# create output tables
+
+
+# INMB --------------------------------------------------------------------
 
 out_tab <- table_costeffectiveness(screen.bcea,
                                    wtp_threshold = wtp_threshold)
 
 print(out_tab)
+
 write.csv(x = out_tab,
             file = paste(diroutput, "costeffectiveness_table.csv", sep = "/"))
+
+
+
+# tb avoided --------------------------------------------------------------
+
+out_tb_tab <- table_tb_avoided(map(dectree_res, "mc_n.tb_screen"))
+
+print(out_tb_tab)
+
+write.csv(x = out_tb_tab,
+          file = paste(diroutput, "tb_avoided_table.csv", sep = "/"))
