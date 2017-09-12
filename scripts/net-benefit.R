6af0fac150e5eae528ba66c892332c2e8c0080e4
Dr Nathan Green <nathan.green@imperial.ac.uk>
2017-09-12 13:32:37 +0100

    * calc net monetary benefit * calc (bayesian) regressions * create output tables

--- scripts/net-benefit.R
+++ scripts/net-benefit.R
@@ -1,11 +1,12 @@
 # ************************************
 # LTBI screening
 # N Green
+# Sept 2017
 #
-# net benefit (not incremental)
+# net benefit regression (not incremental)
 
 
-net_benefit <- function(e_list, c_list) {
+net_benefit <- function(e_list, c_list, wtp_threshold) {
 
   mapply(FUN = function(e, c, wtp) (e * wtp) - c,
          e = e_list,
@@ -14,38 +15,113 @@ net_benefit <- function(e_list, c_list) {
          SIMPLIFY = FALSE)
 }
 
+# create long array over multiple wtp
+nmb <- function(e_statusquo, c_statusquo,
+                e_screened, c_screened,
+                wtp_threshold) {
+
+  list(statusquo = net_benefit(e_list = e_statusquo,
+                               c_list = c_statusquo,
+                               wtp_threshold),
+       screened = net_benefit(e_list = e_screened,
+                              c_list = c_screened,
+                              wtp_threshold)) %>%
+    reshape2::melt() %>%
+    set_names(c("NMB", "scenario", "policy")) %>%
+    mutate(wtp = wtp_threshold)
+}
+
+# partial linear regression function
+lm_wtp <- function(nmb_formula,
+                   design_matrix, ...) {
+  function(threshold){
+
+    lm(nmb_formula,
+       data = subset(design_matrix, wtp == threshold))
+  }
+}
+
+# partial bayesian linear regression function
+bayeslm_wtp <- function(nmb_formula,
+                        design_matrix, ...) {
+  function(threshold){
+    arm::bayesglm(formula = nmb_formula,
+                  family = gaussian,
+                  data = subset(design_matrix, wtp == threshold),
+                  prior.mean = 0, prior.scale = Inf, prior.df = Inf)
+  }
+}
+
+
+design_matrix <- apply(scenario_parameter_p, 2, as.factor)
+
 e_screened <- map2(aTB_CE_stats$QALY.screened_person,
                    map(dectree_res, "mc_health"), `-`)
 
 c_screened <- map2(aTB_CE_stats$cost.screened_person,
                    map(dectree_res, "mc_cost"), `+`)
 
-net_monetary_benefit <- list(statusquo = net_benefit(e_list = aTB_CE_stats$QALY.statusquo_person,
-                                                     c_list = aTB_CE_stats$cost.statusquo_person),
-                             screened = net_benefit(e_list = e_screened,
-                                                    c_list = c_screened))
-net_monetary_benefit_long <-
-  reshape2::melt(net_monetary_benefit) %>%
-  set_names(c("NMB", "scenario", "policy"))
+e_statusquo <- aTB_CE_stats$QALY.statusquo_person
+c_statusquo <- aTB_CE_stats$cost.statusquo_person
 
-design_matrix <- merge(x = params,
-                       y = net_monetary_benefit_long,
+wtp_seq <- seq(10000, 15000, by = 250)
+
+nmb_long <-
+  lapply(wtp_seq,
+         FUN = function(wtp) nmb(e_statusquo, c_statusquo,
+                                 e_screened, c_screened,
+                                 wtp)) %>%
+  do.call(what = rbind, args = .)
+
+design_matrix <- merge(x = design_matrix,
+                       y = nmb_long,
                        by = "scenario")
 
+# simplify names
+names(design_matrix) <- gsub(pattern =  " Treatment", replacement = "", names(design_matrix))
+names(design_matrix) <- gsub(pattern =  " to Screen", replacement = "", names(design_matrix))
+
+# set baseline
+design_matrix <- within(design_matrix,
+                        policy <- factor(policy, levels = c("statusquo", "screened")))
+
+
+# basic model -------------------------------------------------------------
+
+# frequentist
+# lm_basic_wtp <- lm_wtp(as.formula(NMB ~ policy), design_matrix)
+# bayesian
+lm_basic_wtp <- bayeslm_wtp(as.formula(NMB ~ policy), design_matrix)
+
+lm_basic <- lapply(wtp_seq, lm_basic_wtp)
+
+beta_pos <- sapply(lm_basic,
+                   function(x) summary(x)$coefficients["policyscreened", "Estimate"] > 0)
+
+pvalue <- sapply(lm_basic,
+                 function(x) summary(x)$coefficients["policyscreened", "Pr(>|t|)"])  #2-sided
+
+ceac <- ifelse(beta_pos, 1 - pvalue/2, pvalue/2)  #https://bmchealthservres.biomedcentral.com/articles/10.1186/1472-6963-6-68
+plot(y = ceac, x = wtp_seq, type = "o")
+
+
+# multiple dichotomous covariates -----------------------------------------
+
+nmb_formula <- as.formula(NMB ~ policy*Agree*Start +
+                            policy*Agree*Complete +
+                            policy*Agree*Effective +
+                            policy*Start*Complete +
+                            policy*Start*Effective +
+                            policy*Complete*Effective)
+
+# lm_multi_wtp <- lm_wtp(nmb_formula, design_matrix)
+lm_multi_wtp <- bayeslm_wtp(nmb_formula, design_matrix)
+
+lm_multi <- lapply(wtp_seq, lm_multi_wtp)
+
+
+
 ##TODO:
-# do this for each scenario
-
-# basic model
-lm(NMB ~ screen)
-
-# dichotomous covariates
-lm(NMB ~ screen*agree*start +
-          screen*agree*complete +
-          screen*agree*effective +
-          screen*start*complete +
-          screen*start*effective +
-          screen*complete*effective)
-
 # continuous covariates
 # lm(NMB ~ screen*agree*start +
 #      screen*agree*complete +
@@ -53,7 +129,30 @@ lm(NMB ~ screen*agree*start +
 #      screen*start*complete +
 #      screen*start*effective +
 #      screen*complete*effective)
-
-# repeat for different wtp
 #
-# get ceac from regression results
+
+
+# output tables -----------------------------------------------------------
+
+library(broom)
+library(stargazer)
+
+# INMB estimate
+lapply(lm_basic, function(x) tidy(x)[2, ]) %>%
+  do.call(rbind, .)
+
+lapply(lm_multi, function(x) tidy(x)) %>%
+  join_all(by = "term")
+
+# doesnt like bayesglm
+stargazer(lm_basic,
+          type = "text",
+          column.labels = as.character(wtp_seq))
+
+stargazer(lm_multi,
+          type = "text",
+          column.labels = as.character(wtp_seq))
+
+
+cat(paste(xx, collapse = "\n"), "\n", file = "output/lm_table.txt", append = TRUE)
+
