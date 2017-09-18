51797104c1b88c45f4c558a95a4fe65cac75e67c
Dr Nathan Green <nathan.green@imperial.ac.uk>
2017-09-18 13:23:50 +0100

    commit before delete all other ways of estimating time to event curve

--- scripts/04a_1-active-TB-extrapolation.R
+++ scripts/04a_1-active-TB-extrapolation.R
@@ -44,57 +44,66 @@ trans_mat_cens_exituk <-
 activetb_year_pmf_sutherland <-
   activetb_year_pmf_sutherland/sum(activetb_year_pmf_sutherland)*LIFETIME_RISK
 
+
+
+
+
+# survival analysis -------------------------------------------------------
+
 # create final state vectors full sample
-event <- rep(0, nrow(IMPUTED_sample))                          #event-free i.e. censored at followup
-event[IMPUTED_sample$death1] <- 3
-event[IMPUTED_sample$exit_uk1] <- 2
-event[IMPUTED_sample$uk_tb_orig == "1"] <- 1
-
-# etm:: format
-# id from to time_days time
-# 4   9   2  1436.080    4
-# 14  9   0  1961.000    6
-
-data_etm <-
-  data.frame(id = seq_len(nrow(IMPUTED_sample)),
-             from = 9,
-             to = event,
-             time_days = IMPUTED_sample$fup_issdt_days,
-             time = as.numeric(IMPUTED_sample$fup_issdt),
-             LTBI_or_activeTB = IMPUTED_sample$LTBI_or_activeTB) %>%
-  dplyr::filter(LTBI_or_activeTB == TRUE,
-                time > 0) %>%
-  dplyr::select(-LTBI_or_activeTB)
-
-data_etm_cens_exituk <-
-  data_etm %>%
-  dplyr::mutate(to = ifelse(to == 2, 0, to))
-
-# empirical transition probabilities --------------------------------------
-
-res_etm_cens_exituk <- etm::etm(data = data_etm_cens_exituk,
-                                state.names = c(9, 1, 3),
-                                cens.name = 0,
-                                tra = trans_mat_cens_exituk,
-                                s = 0)
-
-res_etm_cmprsk_exituk <- etm::etm(data = data_etm,
-                                  state.names = c(9, 1, 2, 3),
-                                  cens.name = 0,
-                                  tra = trans_mat_cmprsk_exituk,
-                                  s = 0)
-
-# cdf -> pmf
-year_prob.activetb_cens_exituk <-
-  c(0, res_etm_cens_exituk$est["9", "1", ]) %>%
-  diff()
-
-year_prob.activetb_cmprsk_exituk <-
-  c(0, res_etm_cmprsk_exituk$est["9", "1", ]) %>%
-  diff()
+# event <- rep(0, nrow(IMPUTED_sample))                          #event-free i.e. censored at followup
+# event[IMPUTED_sample$death1] <- 3
+# event[IMPUTED_sample$exit_uk1] <- 2
+# event[IMPUTED_sample$uk_tb_orig == "1"] <- 1
+#
+# # etm:: format
+# # id from to time_days time
+# # 4   9   2  1436.080    4
+# # 14  9   0  1961.000    6
+#
+# data_etm <-
+#   data.frame(id = seq_len(nrow(IMPUTED_sample)),
+#              from = 9,
+#              to = event,
+#              time_days = IMPUTED_sample$fup_issdt_days,
+#              time = as.numeric(IMPUTED_sample$fup_issdt),
+#              LTBI_or_activeTB = IMPUTED_sample$LTBI_or_activeTB) %>%
+#   dplyr::filter(LTBI_or_activeTB == TRUE,
+#                 time > 0) %>%
+#   dplyr::select(-LTBI_or_activeTB)
+#
+# data_etm_cens_exituk <-
+#   data_etm %>%
+#   dplyr::mutate(to = ifelse(to == 2, 0, to))
+#
+# # empirical transition probabilities --------------------------------------
+#
+# res_etm_cens_exituk <- etm::etm(data = data_etm_cens_exituk,
+#                                 state.names = c(9, 1, 3),
+#                                 cens.name = 0,
+#                                 tra = trans_mat_cens_exituk,
+#                                 s = 0)
+#
+# res_etm_cmprsk_exituk <- etm::etm(data = data_etm,
+#                                   state.names = c(9, 1, 2, 3),
+#                                   cens.name = 0,
+#                                   tra = trans_mat_cmprsk_exituk,
+#                                   s = 0)
+#
+# # cdf -> pmf
+# year_prob.activetb_cens_exituk <-
+#   c(0, res_etm_cens_exituk$est["9", "1", ]) %>%
+#   diff()
+#
+# year_prob.activetb_cmprsk_exituk <-
+#   c(0, res_etm_cmprsk_exituk$est["9", "1", ]) %>%
+#   diff()
 
 
 # fit exponential distn to trans probs & extrapolate  ----------------------------
+
+year_prob.activetb_cmprsk_exituk_0 <- year_prob.activetb_cmprsk_exituk
+year_prob.activetb_cmprsk_exituk <- incidence_Lancet$mean/100000
 
 max_years_obs <- length(year_prob.activetb_cmprsk_exituk)
 
@@ -103,7 +112,7 @@ max_years_obs <- length(year_prob.activetb_cmprsk_exituk)
 offset <- 0 #0.001
 
 # only use decreasing section of curve
-decreasing_years <- seq(from = 7,
+decreasing_years <- seq(from = 4,
                         to = max_years_obs)
 
 extrap_years <- seq(from = max_years_obs + 1,
