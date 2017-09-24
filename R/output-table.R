babeee32685af92f729eb27340e898f9d3cdec9b
Dr Nathan Green <nathan.green@imperial.ac.uk>
2017-09-24 13:48:21 +0100

    extend the output table function to take a single scenario

--- R/output-table.R
+++ R/output-table.R
@@ -12,16 +12,29 @@
 table_costeffectiveness <- function(bcea_obj,
                                     wtp_threshold = 20000) {
 
-  out <- with(bcea_obj,
-              do.call(data.frame,
-                      list("percentile_5th" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.05),
-                           "EINB" = eib[k == wtp_threshold, ],
-                           "percentile_95th" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.95),
-                           "ceac_WTP15000" = ceac[k == 15000, ],
-                           "ceac_WTP20000" = ceac[k == 20000, ],
-                           "ceac_WTP25000" = ceac[k == 25000, ],
-                           "ceac_WTP30000" = ceac[k == 30000, ]))
-  )
+  out <-
+    if (bcea_obj$n.comparisons == 1) {
+    with(bcea_obj,
+         do.call(data.frame,
+                 list("percentile_5th" = quantile(x = ib[k == wtp_threshold, ], probs = 0.05),
+                      "EINB" = eib[k == wtp_threshold],
+                      "percentile_95th" = quantile(x = ib[k == wtp_threshold, ], probs = 0.95),
+                      "ceac_WTP15000" = ceac[k == 15000],
+                      "ceac_WTP20000" = ceac[k == 20000],
+                      "ceac_WTP25000" = ceac[k == 25000],
+                      "ceac_WTP30000" = ceac[k == 30000])))
+  }else{
+
+    with(bcea_obj,
+         do.call(data.frame,
+                 list("percentile_5th" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.05),
+                      "EINB" = eib[k == wtp_threshold, ],
+                      "percentile_95th" = apply(ib[k == wtp_threshold, , ], 2, quantile, probs = 0.95),
+                      "ceac_WTP15000" = ceac[k == 15000, ],
+                      "ceac_WTP20000" = ceac[k == 20000, ],
+                      "ceac_WTP25000" = ceac[k == 25000, ],
+                      "ceac_WTP30000" = ceac[k == 30000, ])))
+  }
 
   return(out)
 }
