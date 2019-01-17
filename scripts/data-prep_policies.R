2fca3d420d4d13403484a4cf1f03750d068aad31
Dr Nathan Green <nathan.green@imperial.ac.uk>
2019-01-17 15:57:57 +0000

    tidy policy creating script

--- scripts/data-prep_policies.R
+++ scripts/data-prep_policies.R
@@ -12,35 +12,44 @@
 #' ---
 
 
+# incidence_list ----------------------------------------------------------
 ## WHO incidence groups: subsets
-incidence_list <- list(c("(0,50]", "(50,150]", "(150,250]", "(250,350]", "(350,1e+05]"),
-                       c("(150,250]", "(250,350]", "(350,1e+05]"),
-                       c("(250,350]", "(350,1e+05]"))
-
-## WHO incidence groups: mutually exclusive
-# incidence_list <- list(c("(0,50]", "(50,150]", "(150,250]", "(250,350]", "(350,1e+05]"),
+# incidence_list <-
+#   list(c("(0,50]", "(50,150]", "(150,250]", "(250,350]", "(350,1e+05]"),
+#        c("(150,250]", "(250,350]", "(350,1e+05]"),
+#        c("(250,350]", "(350,1e+05]"))
+# incidence_list <-
+#  list(c("(0,50]", "(50,150]", "(150,250]", "(250,350]", "(350,1e+05]"),
 #                        c("(50,150]"),
 #                        c("(150,250]"),
 #                        c("(250,350]"))
+## everyone only
+incidence_list <-
+  list(c("(0,50]", "(50,150]", "(150,250]", "(250,350]", "(350,1e+05]"))
+# incidence_list <-
+#   list(c("(0,50]", "(50,150]", "(150,250]"),
+#        c("(250,350]", "(350,1e+05]"))
 
-## everyone
-# incidence_list <- list(c("(0,50]", "(50,150]", "(150,250]", "(250,350]", "(350,1e+05]"))
 
-endpoints <- c("death", "exit uk")
+# endpoints ---------------------------------------------------------------
+# endpoints <- c("death", "exit uk")
+endpoints <- "exit uk"
+# endpoints <- "death"
 
+
+# LTBI_test ---------------------------------------------------------------
 # LTBI_test <- c("QFT_GIT", "QFT_plus", "TSPOT")
+
+
+# treatment ---------------------------------------------------------------
 # treatment <- c("LTBI_Tx_3mISORIF", "LTBI_Tx_6mISO")
-
-
-## test:
-# incidence_list <- list(c("(0,50]", "(50,150]", "(150,250]"),
-#                        c("(250,350]", "(350,1e+05]"))
-# endpoints <- c("death")
-
-# LTBI_test <- c("TSPOT")
-LTBI_test <- c("QFT_plus")
-
 treatment <- c("LTBI_Tx_3mISORIF")
+
+
+# LTBI_test ---------------------------------------------------------------
+LTBI_test <- "TSPOT"
+# LTBI_test <- "QFT_plus"
+
 
 create_and_save_policies(incidence_list,
                          endpoints,
